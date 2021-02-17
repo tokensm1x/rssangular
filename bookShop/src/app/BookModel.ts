829f85d64abebddf45239e315b77faab86a0417c
Mike Pateev <tokensmix@gmail.com>
2021-02-17 19:47:10 +0300

    init: create app

---
+++ bookShop/src/app/BookModel.ts
@@ -0,0 +1,7 @@
+export interface BookModel {
+  name: string;
+  description: string;
+  price: number;
+  createDate: number;
+  isAvailable: boolean;
+}
---
+++ bookShop/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookComponentComponent } from './book-component/book-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
